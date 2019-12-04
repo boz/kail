e1c7c6c38b0500b3739c4e6ba22d908ebdf60027
paulbdavis <paul@dangersalad.com>
2019-12-03 19:19:59 -0700

    add output mode flag

--- cmd/kail/main.go
+++ cmd/kail/main.go
@@ -44,6 +44,12 @@ var (
 	flagNode       = kingpin.Flag("node", "node").PlaceHolder("NAME").Strings()
 	flagIng        = kingpin.Flag("ing", "ingress").PlaceHolder("NAME").Strings()
 
+	flagOutput = kingpin.Flag("output", "Log output mode (default, raw, or json)").
+			Short('o').
+			PlaceHolder("default").
+			Default("default").
+			String()
+
 	flagContext = kingpin.Flag("context", "kubernetes context").PlaceHolder("CONTEXT-NAME").String()
 
 	flagCurrentNS = kingpin.Flag("current-ns", "use namespace from current context").
@@ -301,7 +307,18 @@ func createController(
 
 func streamLogs(controller kail.Controller) {
 
-	writer := kail.NewWriter(os.Stdout)
+	var writer kail.Writer
+
+	switch *flagOutput {
+	case "default":
+		writer = kail.NewWriter(os.Stdout)
+	case "json":
+		writer = kail.NewJSONWriter(os.Stdout)
+	case "raw":
+		writer = kail.NewRawWriter(os.Stdout)
+	default:
+		kingpin.Fatalf("Invalid output: '%v'", *flagOutput)
+	}
 
 	for {
 		select {
